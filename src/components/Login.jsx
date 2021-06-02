597699dfae2e4a487bf96398920ad957896f3e99
Ydannel <44483665+Ydannel@users.noreply.github.com>
2021-06-02 15:07:26 -0400

    create a context file
    .

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import {GoogleOutlined, FacebookOutlined} from '@ant-design/icons';
+import "firebase/app";
+
+import {auth} from '../firebase';
+import firebase from 'firebase/app';
 
 const Login = () => {
     return ( 
@@ -8,12 +12,12 @@ const Login = () => {
             <div id="login-card">
                 <h2>Welcome to gaia Planet</h2>
 
-                <div className="login-button google">
+                <div className="login-button google" onClick={ ()=> auth.signInWithRedirect( new firebase.auth.GoogleAuthProvider())}>
                     <GoogleOutlined/> Sing in with google
                 </div>
                 <br /> <br />
 
-                <div className="login-button google">
+                <div className="login-button google" onClick={ ()=> auth.signInWithRedirect( new firebase.auth.FacebookAuthProvider())}>
                     <FacebookOutlined/> Sing in with facebook
                 </div>
             </div>

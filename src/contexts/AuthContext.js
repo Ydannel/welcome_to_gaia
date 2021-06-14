af4b31770e59d49ff955af023d19dfc4e27253bf
Ydannel <44483665+Ydannel@users.noreply.github.com>
2021-06-14 10:21:01 -0400

    .
    .

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,3 +1,31 @@
 import React, { useContext, useEffect, useState} from 'react';
 import {useHistory} from 'react-router-dom';
 import {auth} from '../firebase';
+
+const AuthContext = React.createContext();
+export const useAuth =  () => useContext(AuthContext);
+
+
+//provider
+
+export const AuthProvider = ({children}) =>{
+    const [loading, setLoading] = useState(true);
+    const [user, setUser] = useState({});
+    const history = useHistory();
+
+    useEffect(() => {
+        //colect the user data and set it 
+        auth.onAuthStateChanged((user)=>{
+            setUser(user);
+            setLoading(false);
+            history.push('/chats')
+        })
+    }, [user, history])
+    const value = {user};
+    return(   
+        <AuthContext.Provider value={value}>
+            { //if loading is not true, then show the data
+            !loading && children}
+        </AuthContext.Provider>
+    )
+} 
